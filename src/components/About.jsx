b0aa71bdabf24944c9e7eebf37d66ba260e9b5c9
Sayyucha <119330669+Sayyucha@users.noreply.github.com>
2024-09-15 12:39:03 +0530

    new change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <div className='mt-4 md:mt-0 text-left flex   py-6 md:py-0'>
                 <div className='my-auto mx-6'>
                     <h2 className='text-4xl font-bold mb-4 text-red-800'>About Me</h2>
-                    <p className='text-base lg:text-lg'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores fuga, nulla consectetur dicta delectus voluptate voluptatem. Quam distinctio quaerat obcaecati veniam velit pariatur, dolor natus possimus, itaque, assumenda voluptates nobis.</p>
+                    <p className='text-base lg:text-lg'>MY NAME IS SAYYID</p>
                 </div>
             </div>
             <div>
